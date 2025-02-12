628584d0819a8ab67ac0f979d728c09d15ba0879
Robert Delaney <Doggybootsy@gmail.com>
2025-02-11 16:36:29 -0800

    fix

--- dist/EnhanceCodeBlocks.plugin.js
+++ dist/EnhanceCodeBlocks.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name enhancecodeblocks
  * @description Enhances Discords Codeblocks & Text File Attachments
- * @version 1.0.24
+ * @version 1.0.25
  * @author Doggybootsy
  */
 "use strict";
